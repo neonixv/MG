5a767c7566c52dec6a4eaac43daf6c3dede9f70f
Yoni Therese Villamor <yonixv@gmail.com>
2013-05-22 13:52:24 +1200

    new summary without iter flag

--- Metagenomics/src/test/Summary.java
+++ Metagenomics/src/test/Summary.java
@@ -14,40 +14,35 @@ public class Summary {
 			Scanner in = new Scanner(new File(args[0]));
 			int iter = Integer.parseInt(args[1]);
 			System.out.println(Arrays.toString(args));
+			int[] counts = new int[4];
 			while (in.hasNextLine()) {
 				String line = in.nextLine();
-				// beginning init
-				if (line.charAt(0) == '[') {
-					System.out.println(line);
-					iteration:
-					for (int i = 0; i < iter && in.hasNextLine(); i++) {
-						int[] counts = new int[4];
-						do {
-							line = in.nextLine();
-							line = line.trim();
-							if (line.charAt(0) == '0') {
-								if (line.charAt(line.length() - 1) == '0') {
-									counts[0]++;
-								} else if (line.charAt(line.length() - 1) == '1') {
-									counts[1]++;
-								}
-							} else if (line.charAt(0) == '1') {
-								if (line.charAt(line.length() - 1) == '0') {
-									counts[2]++;
-								} else if (line.charAt(line.length() - 1) == '1') {
-									counts[3]++;
-								}
-							} else if (line.charAt(0) != '-'){
-								break iteration;
-							}
-						} while (line.charAt(0) != '-' && in.hasNextLine());
-						System.out.println(line);
+				if (line.charAt(0) == '\t') {
+					line = line.trim();
+					if (line.charAt(0) == '0') {
+						if (line.charAt(line.length() - 1) == '0') {
+							counts[0]++;
+						} else if (line.charAt(line.length() - 1) == '1') {
+							counts[1]++;
+						}
+					} else if (line.charAt(0) == '1') {
+						if (line.charAt(line.length() - 1) == '0') {
+							counts[2]++;
+						} else if (line.charAt(line.length() - 1) == '1') {
+							counts[3]++;
+						}
+					}
+				} else if (line.charAt(0) == '['
+						|| (line.length() > 5 && line.substring(0, 5).equals(
+								"----M"))) {
+					// print and reset counts
+					if (counts[0] > 0) {
 						System.out.println(Arrays.toString(counts));
 						System.out.printf("%.4f\n", fMeasure(counts));
-						if (i > 0)
-							System.out.println(in.nextLine());
+						counts = new int[4];
 					}
-				} else if (line.charAt(0) == 'D' || line.charAt(0) == 'R') {
+					System.out.println(line);
+				} else {
 					System.out.println(line);
 				}
 			}
